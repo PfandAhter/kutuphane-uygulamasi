bf1bb09e7b320a13e7b9b883808be669981df776
Ataberk Bakir <bakirr.ataberk@gmail.com>
2025-11-27 11:32:31 +0300

    UPDATE reactStrictMode updated to false.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: false,
+    async rewrites(){
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:5066'
+            },
+        ];
+    }
 };
-
 export default nextConfig;
