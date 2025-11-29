c5e84a74e2e9d6630af85135d5e5e6529b3fcc20
Ataberk Bakir <bakirr.ataberk@gmail.com>
2025-11-29 17:59:43 +0300

    ADD Route files for Rooms and Shelves (Proxy)

--- app/api/shelf/create/route.ts
+++ app/api/shelf/create/route.ts
@@ -5,13 +5,13 @@ const API_BASE_URL = process.env.BACKEND_PUBLIC_API_BASE_URL;
 
 export async function POST(request: NextRequest) {
     try {
-        console.log("Proxy POST /api/room/add called");
+        console.log("Proxy POST /api/shelf/add called");
 
         const authHeader = request.headers.get("Authorization");
 
         const body = await request.json();
 
-        const response = await axios.post(`${API_BASE_URL}/api/Room`, body, {
+        const response = await axios.post(`${API_BASE_URL}/api/Shelf`, body, {
             headers: {
                 "Content-Type": "application/json",
                 ...(authHeader && { "Authorization": authHeader })
@@ -21,10 +21,11 @@ export async function POST(request: NextRequest) {
         return NextResponse.json(response.data, { status: 201 });
 
     } catch (err: any) {
-        console.error("Proxy Room Add Error:", err?.message);
+        console.error("Proxy Shelf Add Error:", err?.message);
 
         const status = err.response?.status || 500;
-        const data = err.response?.data || { error: "Room creation failed" };
+        const data = err.response?.data || { error: "Shelf creation failed" };
+
         return NextResponse.json(data, { status: status });
     }
 }
