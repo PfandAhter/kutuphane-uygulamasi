5e1e4e4c688940b0de5c1c4778376e4509afd6f1
Ataberk Bakir <bakirr.ataberk@gmail.com>
2025-11-30 21:43:40 +0300

    ADD Proxy for Author (create, delete, get-by-name, list/pageable)

---
+++ app/api/author/get-by-name/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server';
+import axios from 'axios';
+
+const API_BASE_URL = process.env.BACKEND_PUBLIC_API_BASE_URL;
+
+export async function GET(request: NextRequest) {
+    try {
+        const { searchParams } = new URL(request.url);
+        const firstName = searchParams.get('firstName');
+        const lastName = searchParams.get('lastName');
+        const authHeader = request.headers.get("Authorization");
+
+        console.log("Proxy GET /api/publisher/get-by-name called for Name: ", firstName, lastName);
+        const response = await axios.get(`${API_BASE_URL}/api/Authors/by-name`, {
+            params: { firstName: firstName, lastName: lastName },
+            headers: {
+                "Content-Type": "application/json",
+                ...(authHeader && { "Authorization": authHeader })
+            }
+        });
+
+        console.log("Proxy GET /api/publisher/get-by-name successful for Name:", firstName, lastName);
+        return NextResponse.json(response.data, { status: 200 });
+
+    } catch (err: any) {
+        console.error("Proxy Publisher Get By Name Error:", err?.message);
+
+        const status = err.response?.status || 500;
+        const errorMessage = err.response?.data || { error: "Publisher get-by-name failed" };
+        return NextResponse.json(errorMessage, { status: status });
+    }
+}
